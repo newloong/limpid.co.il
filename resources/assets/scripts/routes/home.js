29d64c67032aae1e4f3114a47bca38701eea7bc4
Alvin Manansala <aomanansala@gmail.com>
2018-09-01 18:52:32 +0800

    fix portfolio

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -18,49 +18,7 @@ export default {
       },
     });
 
-    var $container = $('.portfolio-container');
-
-    $('.portfolio-filter a').click(function() {
-      $('.portfolio-filter .current').removeClass('current');
-      $(this).addClass('current');
-
-      var selector = $(this).attr('data-filter');
-      $container.isotope({
-        isOriginLeft: false,
-        filter: selector,
-        animationOptions: {
-          duration: 750,
-          easing: 'linear',
-          queue: false,
-        },
-      });
-      return false;
-    });
-
-    $('.portfolio-filter a:first-child').trigger('click');
-    $('.portfolio-filter a:first-child').trigger('click');
   },
   finalize() {
-    var $container = $('.portfolio-container');
-
-    $('.portfolio-filter a').click(function() {
-      $('.portfolio-filter .current').removeClass('current');
-      $(this).addClass('current');
-
-      var selector = $(this).attr('data-filter');
-      $container.isotope({
-        isOriginLeft: false,
-        filter: selector,
-        animationOptions: {
-          duration: 750,
-          easing: 'linear',
-          queue: false,
-        },
-      });
-      return false;
-    });
-
-    $('.portfolio-filter a:first-child').trigger('click');
-    $('.portfolio-filter a:first-child').trigger('click');
   },
 };
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -29,7 +29,26 @@
 
 
 <script>
-    jQuery(document).ready(function($) {
+    $(window).load(function() {
+        var $container = $('.portfolio-container');
+
+        $('.portfolio-filter a').click(function() {
+          $('.portfolio-filter .current').removeClass('current');
+          $(this).addClass('current');
+
+          var selector = $(this).attr('data-filter');
+          $container.isotope({
+            isOriginLeft: false,
+            filter: selector,
+            animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false,
+            },
+          });
+          return false;
+        });
+
         $('.portfolio-filter a:first-child').trigger('click');
     })
 </script>
